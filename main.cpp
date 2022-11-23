ec217b2de4ed36c535783e1f9876063b78272c5a
JaviVi <javicente@deusto.es>
2022-11-23 19:32:00 +0100

    c

--- main.cpp
+++ main.cpp
@@ -5,27 +5,18 @@
 
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500
+#define WAIT_TIME_MS 100
 CAN can1(PB_8, PB_9, 500000);
-char counter = 0;
-char txBuff[8];
+
 unsigned int txID = 1337;
 CANMessage txMsg;
 CANMessage rxMsg;
 void printMessage(CANMessage msg);
 
 int main() {
-  printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n",
-         MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
   while (true) {
-    txBuff[0] = counter;
-    txMsg = CANMessage(txID, txBuff, 8);
-
-    if (can1.write(txMsg)) {
-      counter++;
-      printf("Message sent: %d\n", counter);
-    }
+   
     if (can1.read(rxMsg)) {
       printMessage(rxMsg);
     }
